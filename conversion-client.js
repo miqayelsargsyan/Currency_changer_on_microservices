2770b8481528e60d0690d5054a8c72b998822535
misha <mikael_2000@mail.ru>
2018-04-04 18:06:04 +0400

    adding express to the app

--- conversion-client.js
+++ conversion-client.js
@@ -1,10 +1,24 @@
 const cote = require('cote');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-const requester = new cote.Requester({name: 'currency conversion requester'});
+const app = express();
+app.use(bodyParser.json());
+let port = 3000;
 
-const request = { type: 'convert', from: 'usd', to: 'eur', amount: 100 };
+app.post('/api/convert', (req, res) => {
+    const requester = new cote.Requester({name: 'currency conversion requester'});
+
+    const request = { type: 'convert', from: 'usd', to: 'eur', amount: req.body.amount };
+    
+    
+    requester.send(request, (res) => {
+        console.log(res)
+    })
+})
 
 
-requester.send(request, (res) => {
-    console.log(res)
+
+app.listen(port, () => {
+    console.log(`The server is up on port ${port}`)
 })
