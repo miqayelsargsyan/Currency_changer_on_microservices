a15a687887f4b3efe088a5da2dc9f6ea976ad4b7
misha <mikael_2000@mail.ru>
2018-04-04 17:53:29 +0400

    started project on microservices

---
+++ conversion-service.js
@@ -0,0 +1,13 @@
+const cote = require('cote');
+
+const responder = new cote.Responder({ name: 'currency conversion responder' });
+const subscriber = new cote.Subscriber({ name: 'arbitration subscriber' });
+const rates = { usd_eur: 0.90, eur_usd: 1.10 };
+
+subscriber.on('update rate', (update) => {
+    rates[update.currencies] = update.rate;
+});
+
+responder.on('convert', (req, callback) => {
+    callback(req.amount * rates[`${req.from}_${req.to}`])
+})
