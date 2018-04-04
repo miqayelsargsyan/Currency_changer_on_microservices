b02fa9f475fdc0d55349001076ba9155f0e19b0b
misha <mikael_2000@mail.ru>
2018-04-04 19:30:44 +0400

    added currencies API

--- conversion-client.js
+++ conversion-client.js
@@ -9,11 +9,11 @@ let port = 3000;
 app.post('/api/convert', (req, res) => {
     const requester = new cote.Requester({name: 'currency conversion requester'});
 
-    const request = { type: 'convert', from: 'usd', to: 'eur', amount: req.body.amount };
+    let request = { type: 'convert', from: req.body.from, to: req.body.to, amount: req.body.amount };
     
     
-    requester.send(request, (res) => {
-        console.log(res)
+    requester.send(request, (result) => {
+        res.send(` ${req.body.amount} ${req.body.from} is ${result} ${req.body.to}`)
     })
 })
 
--- conversion-service.js
+++ conversion-service.js
@@ -1,8 +1,20 @@
 const cote = require('cote');
-
+const request = require('request');
+const bodyParser = require('body-parser');
 const responder = new cote.Responder({ name: 'currency conversion responder' });
 const subscriber = new cote.Subscriber({ name: 'arbitration subscriber' });
-const rates = { usd_eur: 0.90, eur_usd: 1.10 };
+
+const key = '6002a6eedee7b56e4b903d4d878e6bb5';
+
+let rates = {};
+
+    request({
+        url: `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,AED`,
+        json: true
+    }, ( error, response, body ) => {
+         rates = { eur_usd: body.rates.USD, eur_aed: body.rates.AED };
+    })
+
 
 subscriber.on('update rate', (update) => {
     rates[update.currencies] = update.rate;
